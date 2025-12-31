3314911bce1241331b1c73e67af62a5a7ea49a23
Sammy <sammy@kozykoding.com>
2025-12-30 18:11:07 -0800

    added to call function and updated main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 
 load_dotenv()
@@ -30,23 +30,34 @@ def main():
         ),
     )
     if args.verbose:
-        if not response.function_calls:
-            print(f"User prompt: {args.user_prompt}")
-            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-            print("Response:")
-            print(response.text)
-        else:
-            for function_call in response.function_calls:
-                print(f"Calling function: {function_call.name}({function_call.args})")
+        print(f"User prompt: {args.user_prompt}\n")
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-    else:
-        if not response.function_calls:
-            print("Response:")
-            print(response.text)
-        else:
-            for function_call in response.function_calls:
-                print(f"Calling function: {function_call.name}({function_call.args})")
+    if not response.function_calls:
+        print("Response:")
+        print(response.text)
+        return
+
+    function_responses = []
+    for function_call in response.function_calls:
+        # actually call the function
+        function_call_result = call_function(function_call, args.verbose)
+
+        # validate the result
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+            or not function_call_result.parts[0].function_response.response
+        ):
+            raise RuntimeError(f"Empty function response for {function_call.name}")
+
+        # print the result in verbose mode
+        if args.verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+
+        # store the part
+        function_responses.append(function_call_result.parts[0])
 
 
 if __name__ == "__main__":
